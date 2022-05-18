47b5f80d5e1582119878ce3776f6931bd23a59e5
Sultnkh <47182157+Sultnkh@users.noreply.github.com>
2022-05-18 13:24:00 +0300

    Create LoopDLoop.js

---
+++ LoopDLoop.js
@@ -0,0 +1,14 @@
+/*var Miles=0;
+for(Miles=0;Miles<=6;Miles++){
+    if(Miles%2==0){
+        console.log("Here's a piece of candy!")
+    }
+}*/
+
+var Miles=0;
+var speed=5.8;
+for(Miles=0;Miles<6;Miles++){
+    if(Miles%2==0 &&speed>5.5){
+        console.log("Here's a piece of candy!")
+    }
+}
